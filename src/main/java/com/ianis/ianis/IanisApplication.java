9a233f7a4f250311ab84c3442ac078b0af3cae74
ianis <smarthab@mbp-de-smarthab.home>
2020-06-12 11:01:34 +0200

    generate project

---
+++ src/main/java/com/ianis/ianis/IanisApplication.java
@@ -0,0 +1,13 @@
+package com.ianis.ianis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IanisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IanisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ianis/ianis/IanisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ianis.ianis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IanisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
